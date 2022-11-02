47cc1ad993dd6df438f6802a8ecd9115c9259854
zhaowd <202861616@qq.com>
2022-11-02 15:22:43 +0800

    项目和资源对接起来

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports ={
     //  '/dist/'表示打包后项目的名称
-   publicPath:  process.env.NODE_ENV === 'production'? '/docs/': '/', // 公共路径(必须有的)
+   publicPath:  process.env.NODE_ENV === 'production'? '': '/', // 公共路径(必须有的)
    outputDir: "docs", // 输出文件目录
    assetsDir: "static"//静态资源文件名称
    }
