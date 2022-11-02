ba9c959ca7cea7d1286579546a2ea3f21bb37265
zhaowd <202861616@qq.com>
2022-11-02 14:59:32 +0800

    docs

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports ={
     //  '/dist/'表示打包后项目的名称
-   publicPath:  process.env.NODE_ENV === 'production'? '/dist/': '/', // 公共路径(必须有的)
-   outputDir: "dist", // 输出文件目录
+   publicPath:  process.env.NODE_ENV === 'production'? '/docs/': '/', // 公共路径(必须有的)
+   outputDir: "docs", // 输出文件目录
    assetsDir: "static"//静态资源文件名称
    }
